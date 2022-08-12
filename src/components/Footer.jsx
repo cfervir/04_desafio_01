632a6d3e030f3f85b5860cadb121d21bee092d31
Carlos Fernández <carlos@bigbadlion.com>
2022-08-12 02:32:40 -0400

    Correción de typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <p className="footer--title">Galería de distintos felinos por el mundo</p>
       <Button href="https://www.google.com" variant="success" target="_blank">Revisa más acá</Button>
       <p className="footer--derechos">2022 - Todos los derechos reservados.</p>
-      <p className="footer--legal">Imágenes obtenitas de Unsplash.com</p>
+      <p className="footer--legal">Imágenes obtenidas de Unsplash.com</p>
     </footer>
   );
 };
